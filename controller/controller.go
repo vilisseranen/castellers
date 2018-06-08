bee15d9c7062f976e6b8341e6a72b553a08c506b
Clément Contini <vilisseranen@gmail.com>
2018-06-07 22:51:35 -0400

    move controller to separate package

---
+++ controller/controller.go
@@ -0,0 +1,204 @@
+package controller
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/vilisseranen/castellers/model"
+)
+
+func GetEvent(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	uuid := vars["uuid"]
+	e := model.Event{UUID: uuid}
+	if err := e.Get(); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "Event not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	respondWithJSON(w, http.StatusOK, e)
+}
+
+func GetMember(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	uuid := vars["uuid"]
+	m := model.Member{UUID: uuid}
+	if err := m.Get(); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "Member not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	respondWithJSON(w, http.StatusOK, m)
+}
+
+func GetEvents(w http.ResponseWriter, r *http.Request) {
+	count, _ := strconv.Atoi(r.FormValue("count"))
+	start, _ := strconv.Atoi(r.FormValue("start"))
+	if count < 1 {
+		count = 10
+	}
+	e := model.Event{}
+	events, err := e.GetEvents(start, count)
+	if err != nil {
+		switch err {
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	respondWithJSON(w, http.StatusOK, events)
+}
+
+func CreateEvent(w http.ResponseWriter, r *http.Request) {
+	// Check if admin exists
+	vars := mux.Vars(r)
+	uuid := vars["uuid"]
+	admin := model.Admin{UUID: uuid}
+	if err := admin.Get(); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusUnauthorized, "This admin is not authorized to create events.")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	// Create the event
+	var e model.Event
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&e); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+	if err := e.CreateEvent(); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusCreated, e)
+}
+
+func CreateMember(w http.ResponseWriter, r *http.Request) {
+	// Check if admin exists
+	vars := mux.Vars(r)
+	uuid := vars["uuid"]
+	admin := model.Admin{UUID: uuid}
+	if err := admin.Get(); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusUnauthorized, "This admin is not authorized to create members.")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	// Create the event
+	var m model.Member
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&m); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+	if err := m.CreateMember(); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusCreated, m)
+}
+
+func ParticipateEvent(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	event_uuid := vars["event_uuid"]
+	member_uuid := vars["member_uuid"]
+	event := model.Event{UUID: event_uuid}
+	member := model.Member{UUID: member_uuid}
+	if err := member.Get(); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusUnauthorized, "You are not authorized to register for this event.")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	if err := event.Get(); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusUnauthorized, "You are not authorized to register for this event.")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	var p model.Participation
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&p); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	p.EventUUID = event_uuid
+	p.MemberUUID = member_uuid
+
+	if err := p.Participate(); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusCreated, p)
+}
+
+func UpdateEvent(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	uuid := vars["uuid"]
+	var e model.Event
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&e); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+	e.UUID = uuid
+	if err := e.UpdateEvent(); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, e)
+}
+
+func DeleteEvent(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	uuid := vars["uuid"]
+	e := model.Event{UUID: uuid}
+	if err := e.DeleteEvent(); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, nil)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
