adaed0fad31fb1a96080a657871550e757c19201
Clément Contini <vilisseranen@gmail.com>
2018-06-23 14:11:22 -0400

    use static files from ./static

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 func main() {
 	a := App{}
 	//a.Initialize(os.Getenv("APP_DB_NAME"))
-	a.Initialize("test_database.db", "frontend/dist")
+	a.Initialize("test_database.db", "static")
 
 	a.Run(":8080", "./castellers.log")
 }
