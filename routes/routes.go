36b0f3248a3e4f1d5c14c1c8d5ad29508ba4feb4
Clément Contini <vilisseranen@gmail.com>
2018-07-10 21:09:14 -0400

    prefix all routes with /api

--- routes/routes.go
+++ routes/routes.go
@@ -12,21 +12,21 @@ func CreateRouter(staticDir string) *mux.Router {
 	r := mux.NewRouter()
 
 	// No auth
-	r.HandleFunc("/initialize", controller.Initialize).Methods("POST")
-	r.HandleFunc("/initialize", controller.IsInitialized).Methods("GET")
-	r.HandleFunc("/events", controller.GetEvents).Methods("GET")
-	r.HandleFunc("/events/{uuid:[0-9a-f]+}", controller.GetEvent).Methods("GET")
+	r.HandleFunc("/api/initialize", controller.Initialize).Methods("POST")
+	r.HandleFunc("/api/initialize", controller.IsInitialized).Methods("GET")
+	r.HandleFunc("/api/events", controller.GetEvents).Methods("GET")
+	r.HandleFunc("/api/events/{uuid:[0-9a-f]+}", controller.GetEvent).Methods("GET")
 
 	// Requires member uuid
-	r.HandleFunc("/events/{event_uuid:[0-9a-f]+}/members/{member_uuid:[0-9a-f]+}", checkMember(controller.ParticipateEvent)).Methods("POST")
+	r.HandleFunc("/api/events/{event_uuid:[0-9a-f]+}/members/{member_uuid:[0-9a-f]+}", checkMember(controller.ParticipateEvent)).Methods("POST")
 	r.HandleFunc("/members/{member_uuid:[0-9a-f]+}", checkMember(controller.GetMember)).Methods("GET")
 
 	// Requires admin uuid
-	r.HandleFunc("/admins/{admin_uuid:[0-9a-f]+}/events", checkAdmin(controller.CreateEvent)).Methods("POST")
-	r.HandleFunc("/admins/{admin_uuid:[0-9a-f]+}/members", checkAdmin(controller.CreateMember)).Methods("POST")
-	r.HandleFunc("/admins/{admin_uuid:[0-9a-f]+}/events/{uuid:[0-9a-f]+}", checkAdmin(controller.UpdateEvent)).Methods("PUT")
-	r.HandleFunc("/admins/{admin_uuid:[0-9a-f]+}/events/{uuid:[0-9a-f]+}", checkAdmin(controller.DeleteEvent)).Methods("DELETE")
-	r.HandleFunc("/admins/{admin_uuid:[0-9a-f]+}/events/{event_uuid:[0-9a-f]+}/members/{member_uuid:[0-9a-f]+}", checkAdmin(controller.PresenceEvent)).Methods("POST")
+	r.HandleFunc("/api/admins/{admin_uuid:[0-9a-f]+}/events", checkAdmin(controller.CreateEvent)).Methods("POST")
+	r.HandleFunc("/api/admins/{admin_uuid:[0-9a-f]+}/members", checkAdmin(controller.CreateMember)).Methods("POST")
+	r.HandleFunc("/api/admins/{admin_uuid:[0-9a-f]+}/events/{uuid:[0-9a-f]+}", checkAdmin(controller.UpdateEvent)).Methods("PUT")
+	r.HandleFunc("/api/admins/{admin_uuid:[0-9a-f]+}/events/{uuid:[0-9a-f]+}", checkAdmin(controller.DeleteEvent)).Methods("DELETE")
+	r.HandleFunc("/api/admins/{admin_uuid:[0-9a-f]+}/events/{event_uuid:[0-9a-f]+}/members/{member_uuid:[0-9a-f]+}", checkAdmin(controller.PresenceEvent)).Methods("POST")
 
 	// Static site
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir(staticDir)))
