6a4209b04c536e84c00ef6d755046015b2a8c04e
Clément Contini <vilisseranen@gmail.com>
2018-07-10 21:08:19 -0400

    add vuex to configure auth object

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 import App from './App.vue'
 
 // LightBootstrap plugin
@@ -10,6 +11,7 @@ import routes from './routes/routes'
 // plugin setup
 Vue.use(VueRouter)
 Vue.use(LightBootstrap)
+Vue.use(Vuex)
 
 // configure router
 const router = new VueRouter({
@@ -17,11 +19,30 @@ const router = new VueRouter({
   linkActiveClass: 'nav-item active'
 })
 
+// Configure vuex store
+const store = new Vuex.Store({
+  state: {
+    auth: {
+      uuid: '',
+      code: '',
+      type: ''
+    }
+  },
+  mutations: {
+    authenticate (state, payload) {
+      state.auth.uuid = payload.uuid
+      state.auth.code = payload.code
+      state.auth.type = payload.type
+    }
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   render: h => h(App),
   router,
+  store,
   methods: {
     globalRedirect () {
       if ('next' in this.$route.query) {
@@ -29,14 +50,19 @@ new Vue({
         console.log('Redirecting to ' + this.$route.query.next)
       }
     },
-    checkCode () {
-      if ('code' in this.$route.query) {
-        console.log('You provided the code: ' + this.$route.query.code)
+    checkCredentials () {
+      if ('c' in this.$route.query && 'm' in this.$route.query) {
+        this.$store.commit('authenticate', {
+          uuid: this.$route.query.m,
+          code: this.$route.query.c,
+          type: 'admin'
+        })
+        console.log('You are authenticated as : ' + JSON.stringify(this.$store.state.auth))
       }
     }
   },
   mounted () {
-    this.globalRedirect()
-    this.checkCode()
+    this.checkCredentials() // this must be the first thing to do
+    this.globalRedirect()   // you redirect after being authenticated
   }
 })
