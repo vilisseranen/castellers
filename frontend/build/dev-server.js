f24fb4b1c14aba22c2023831122be0b91f3c93c8
vilisseranen <vilisseranen@gmail.com>
2018-09-18 17:26:28 -0400

    listen_all

--- frontend/build/dev-server.js
+++ frontend/build/dev-server.js
@@ -69,7 +69,7 @@ app.use(hotMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var uri = 'http://localhost:' + port
+var uri = 'http://*:' + port
 
 var _resolve
 var readyPromise = new Promise(resolve => {
