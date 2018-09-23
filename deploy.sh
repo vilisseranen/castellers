4941214a171f32cd317a5ec3184f66d02ddbdcc7
vilisseranen <vilisseranen@gmail.com>
2018-09-23 17:57:24 -0400

    add ssh key to agent

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
 docker push vilisseranen/castellers:$1
 
-echo "$TRAVIS_SSH" | base64 -d > travis_ssh
-chmod 600 travis_ssh
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i travis_ssh travis-deploy@clemissa.info -p 274 'uptime'
+echo "$TRAVIS_SSH" | base64 -d > /tmp/travis_ssh
+eval "$(ssh-agent -s)"
+chmod 600 /tmp/travis_ssh
+ssh-add /tmp/travis_ssh
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -n travis-deploy@clemissa.info -p 274 'uptime'
