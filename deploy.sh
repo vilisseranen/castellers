98d2b801670e7101a1b977399826d7be678a698f
vilisseranen <vilisseranen@gmail.com>
2018-09-23 17:01:33 -0400

    use base64 encoded ssh key

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
 docker push vilisseranen/castellers:$1
 
-echo "$TRAVIS_SSH" > travis_ssh
+echo "$TRAVIS_SSH" | base64 -d > travis_ssh
 chmod 600 travis_ssh
 ssh -i travis_ssh travis-deploy@clemissa.info -p 274 'uptime'
