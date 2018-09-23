864ebd5a1187e44ac577295c73cf67f51aac8b95
vilisseranen <vilisseranen@gmail.com>
2018-09-23 17:41:03 -0400

    no remote host checking

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ docker push vilisseranen/castellers:$1
 
 echo "$TRAVIS_SSH" | base64 -d > travis_ssh
 chmod 600 travis_ssh
-ssh -i travis_ssh travis-deploy@clemissa.info -p 274 'uptime'
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i travis_ssh travis-deploy@clemissa.info -p 274 'uptime'
