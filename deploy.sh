aa98998cb6646bfac282f74a72adabf94d54bff7
vilisseranen <vilisseranen@gmail.com>
2018-09-23 20:53:33 -0400

    Use encrypted file for ssh key

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+
+# Push latest image to docker hub
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
 docker push vilisseranen/castellers:$1
 
-echo "$TRAVIS_SSH" | base64 -d > /tmp/travis_ssh
-eval "$(ssh-agent -s)"
-chmod 600 /tmp/travis_ssh
-ssh-add /tmp/travis_ssh
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -n travis-deploy@clemissa.info -p 274 'uptime'
+# Deploy on server
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no travis-deploy@clemissa.info -p 274 'uptime'
