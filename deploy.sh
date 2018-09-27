89c13705fd39271524df5913842fc8bf75695b16
vilisseranen <vilisseranen@gmail.com>
2018-09-26 21:16:22 -0400

    deploy to server

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-
 docker push vilisseranen/castellers:$1
 
 # Deploy on server
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no travis-deploy@clemissa.info -p 274 'uptime'
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no travis-deploy@clemissa.info -p 274 'sudo docker pull vilisseranen/castellers:latest && sudo docker stop castellers && sudo docker rm castellers && sudo docker run --name="castellers" -d -v app_var_log:/var/log -v app_data:/data -v app_etc:/etc/castellers -p 127.0.0.1:8080:8080/tcp vilisseranen/castellers:latest'
