3c76340fb73a2a53928d4f37012a1904960276f4
vilisseranen <vilisseranen@gmail.com>
2018-09-30 16:33:46 -0400

    Send emails with template

--- common/mail.go
+++ common/mail.go
@@ -1,9 +1,63 @@
 package common
 
 import (
-	"github.com/gadelkareem/go-helpers"
+	"bytes"
+	"fmt"
+	"html/template"
+	"net/smtp"
 )
 
-func SendMail(subject, body string, to []string) error {
-	return h.SendMail(GetConfigString("smtp_server"), GetConfigString("mail_from"), subject, body, to)
+func SendRegistrationEmail(to, memberName, adminName, adminExtra, activateLink, profileLink string) error {
+	// Prepare header
+	header := "Subject: Inscription\r\n" +
+		"To: " + to + "\r\n" +
+		"From: " + GetConfigString("mail_from") + "\r\n" +
+		"MIME-version: 1.0;\r\n" +
+		"Content-Type: text/html; charset=\"UTF-8\";\r\n" +
+		"\r\n"
+	// Parse body
+	t, err := template.ParseFiles("templates/email_register_fr.html")
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	buf := new(bytes.Buffer)
+	imageSource := GetConfigString("domain") + "/static/img/"
+	emailInfo := emailRegisterInfo{memberName, adminName, adminExtra, activateLink, profileLink, imageSource}
+	if err = t.Execute(buf, emailInfo); err != nil {
+		fmt.Println(err)
+		return err
+	}
+	body := header + buf.String()
+	// Send mail
+	if err = sendMail([]string{to}, body); err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+type emailRegisterInfo struct {
+	MemberName             string
+	AdminName, AdminExtra  string
+	LoginLink, ProfileLink string
+	ImageSource            string
+}
+
+type email struct {
+	from    string
+	to      []string
+	subject string
+	body    string
+}
+
+func sendMail(to []string, body string) error {
+	var auth smtp.Auth
+	auth = smtp.PlainAuth("", GetConfigString("smtp_username"), GetConfigString("smtp_password"), GetConfigString("smtp_server"))
+	addr := GetConfigString("smtp_server") + ":" + GetConfigString("smtp_port")
+	if err := smtp.SendMail(addr, auth, GetConfigString("mail_from"), to, []byte(body)); err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
 }
