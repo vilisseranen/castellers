68d5076572bdcc50a6c355b97f216253b1cb2f12
vilisseranen <vilisseranen@gmail.com>
2018-10-11 10:58:53 -0400

    fix mobile menu

--- frontend/src/components/Dashboard/Layout/MobileMenu.vue
+++ frontend/src/components/Dashboard/Layout/MobileMenu.vue
@@ -1,9 +1,15 @@
 <template>
   <ul class="nav nav-mobile-menu">
-    <li class="nav-item">
+    <li class="nav-item" v-if="this.uuid">
       <router-link class="nav-link" :to="{ name: 'MemberEdit', params: { uuid: uuid}}">
         <i class="nc-icon nc-circle-09 hidden-lg-up"></i>
         <span> {{ $t('general.profile_zone') }}</span>
+      </router-link>
+    </li>
+    <li class="nav-item" v-if="!this.uuid">
+      <router-link class="nav-link" :to="{ name: 'Login'}">
+        <i class="nc-icon nc-tap-01 hidden-lg-up"></i>
+        <span> {{ $t('general.login') }}</span>
       </router-link>
     </li>
   </ul>
