ab80eed542cf134bb1f4ea36b1959c31a66bc670
Marcel <marcel.farres@gmail.com>
2019-03-08 17:03:32 -0500

    Send using gmail api
    Need testing

--- common/mail.go
+++ common/mail.go
@@ -2,12 +2,70 @@ package common
 
 import (
 	"bytes"
+	"context"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"net/smtp"
+	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/gmail/v1"
 )
 
+var mailService *gmail.Service
+
+func init() {
+	b, err := ioutil.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
+
+	mailService, err = gmail.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve Gmail client: %v", err)
+	}
+}
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from file: %v", err)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
 func SendRegistrationEmail(to, memberName, language, adminName, adminExtra, activateLink, profileLink string) error {
+	var msg gmail.Message
 	// Prepare header
 	header := "Subject: Inscription\r\n" +
 		"To: " + to + "\r\n" +
@@ -23,15 +81,20 @@ func SendRegistrationEmail(to, memberName, language, adminName, adminExtra, acti
 		return err
 	}
 	buf := new(bytes.Buffer)
+	buf.WriteString(header)
 	imageSource := GetConfigString("domain") + "/static/img/"
 	emailInfo := emailRegisterInfo{memberName, adminName, adminExtra, activateLink, profileLink, imageSource}
 	if err = t.Execute(buf, emailInfo); err != nil {
 		fmt.Println("Error generating template: " + err.Error())
 		return err
 	}
-	body := header + buf.String()
+
+	msg.Raw = base64.URLEncoding.EncodeToString(buf.Bytes())
+
+	// Send the message
+	_, err = mailService.Users.Messages.Send("me", &msg).Do()
 	// Send mail
-	if err = sendMail([]string{to}, body); err != nil {
+	if err != nil {
 		fmt.Println("Error sending Email: " + err.Error())
 		return err
 	}
--- go.mod
+++ go.mod
@@ -6,6 +6,13 @@ require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/gorilla/handlers v1.4.0
 	github.com/gorilla/mux v1.7.0
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/mattn/go-sqlite3 v1.10.0
 	github.com/spf13/viper v1.3.1
+	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/net v0.0.0-20190301231341-16b79f2e4e95 // indirect
+	golang.org/x/oauth2 v0.0.0-20190226205417-e64efc72b421
+	golang.org/x/sys v0.0.0-20190308023053-584f3b12f43e // indirect
+	google.golang.org/api v0.1.0
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
