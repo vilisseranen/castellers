f6d2f9ecedff359b7ef8657a4eebef74357e3d1a
Clément Contini <vilisseranen@gmail.com>
2020-02-22 10:36:21 -0500

    add emergency contact to users

--- tests/helper_test.go
+++ tests/helper_test.go
@@ -75,14 +75,14 @@ func (test *TestHelper) addEvent(uuid, name string, startDate, endDate int) {
 }
 
 func (test *TestHelper) addAMember() {
-	test.addMember("deadbeef", "Ramon", "Gerard", "", "", "Cap de rengla", "segon,baix,terç", "member", "ramon@gerard.ca", "toto")
+	test.addMember("deadbeef", "Ramon", "Gerard", "", "", "Cap de rengla", "segon,baix,terç", "member", "ramon@gerard.ca", "", "toto")
 }
 
 func (test *TestHelper) addAnAdmin() {
-	test.addMember("deadfeed", "Romà", "Èric", "", "", "Cap de colla", "baix,second", "admin", "romà@eric.ca", "tutu")
+	test.addMember("deadfeed", "Romà", "Èric", "", "", "Cap de colla", "baix,second", "admin", "romà@eric.ca", "", "tutu")
 }
 
-func (test *TestHelper) addMember(uuid, firstName, lastName, height, weight, extra, roles, memberType, email, code string) {
+func (test *TestHelper) addMember(uuid, firstName, lastName, height, weight, extra, roles, memberType, email, contact, code string) {
 	db, err := sql.Open("sqlite3", testDbName)
 	if err != nil {
 		log.Fatal(err)
@@ -92,12 +92,12 @@ func (test *TestHelper) addMember(uuid, firstName, lastName, height, weight, ext
 	if err != nil {
 		log.Fatal(err)
 	}
-	stmt, err := tx.Prepare("INSERT INTO members(uuid, firstName, lastName, height, weight, roles, extra, type, email, code) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+	stmt, err := tx.Prepare("INSERT INTO members(uuid, firstName, lastName, height, weight, roles, extra, type, email, contact, code) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer stmt.Close()
-	_, err = stmt.Exec(uuid, firstName, lastName, height, weight, roles, extra, memberType, email, code)
+	_, err = stmt.Exec(uuid, firstName, lastName, height, weight, roles, extra, memberType, email, contact, code)
 	if err != nil {
 		log.Fatal(err)
 	}
