7ae7b7d93ce47b27ac90a95d0dd9b8b1be9b4bf7
Clément Contini <vilisseranen@gmail.com>
2020-05-24 10:04:03 -0400

    do not fail if previous container does not run or does not exist

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-
 docker push vilisseranen/castellers:$1
 
 # Deploy on server
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no travis-deploy@clemissa.info -p 274 'sudo docker pull vilisseranen/castellers:latest && sudo docker stop castellers && sudo docker rm castellers && sudo docker run --restart=always --name="castellers" -d -v app_var_log:/var/log -v app_data:/data -v app_etc:/etc/castellers -p 127.0.0.1:8080:8080/tcp vilisseranen/castellers:latest'
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no travis-deploy@clemissa.info -p 274 'sudo docker pull vilisseranen/castellers:latest && sudo docker stop castellers || true && sudo docker rm castellers || true && sudo docker run --restart=always --name="castellers" -d -v app_var_log:/var/log -v app_data:/data -v app_etc:/etc/castellers -p 127.0.0.1:8080:8080/tcp vilisseranen/castellers:latest'
