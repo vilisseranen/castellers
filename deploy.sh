939c3e4902083e639bd55bb3eb58bb5f17b63f0d
Clément Contini <clement.contini@appdirect.com>
2021-02-06 18:59:10 -0500

    pull image before docker-compose up (#66)

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-
 docker push vilisseranen/castellers:$1
 
 # Deploy on server
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no travis-deploy@carl.myhypervisor.ca -p 274 'cd /data/docker-compose/castellers && sudo docker-compose up -d'
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no travis-deploy@carl.myhypervisor.ca -p 274 'cd /data/docker-compose/castellers && sudo docker-compose pull && sudo docker-compose up -d'
