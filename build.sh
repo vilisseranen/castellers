28bb655c6a99923c3626c491ba58b24c057ba54a
Clément Contini <vilisseranen@gmail.com>
2021-02-13 12:54:28 -0500

    removing echo in build cmd

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Building docker image..."
-echo -x 'sudo docker build -t vilisseranen/castellers:latest -t vilisseranen/castellers:latest -t vilisseranen/castellers:`cat VERSION` .'
+sudo docker build -t vilisseranen/castellers:latest -t vilisseranen/castellers:latest -t vilisseranen/castellers:`cat VERSION` .
