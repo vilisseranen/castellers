87cf56d718b3cae2599bf6a95c83e4b0a53eb0d5
Clément Contini <vilisseranen@gmail.com>
2021-02-13 13:07:56 -0500

    tag dev in build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Building docker image..."
-sudo docker build -t vilisseranen/castellers:latest -t vilisseranen/castellers:latest -t vilisseranen/castellers:`cat VERSION` .
+sudo docker build -t vilisseranen/castellers:latest -t vilisseranen/castellers:dev -t vilisseranen/castellers:latest -t vilisseranen/castellers:`cat VERSION` .
