45796cee19147a9ed20fca71eed54e093f660738
Clément Contini <vilisseranen@gmail.com>
2021-02-13 12:49:07 -0500

    tag docker with dev, latest and version

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Building docker image..."
-sudo docker build -t vilisseranen/castellers:$1 .
+echo -x 'sudo docker build -t vilisseranen/castellers:latest -t vilisseranen/castellers:latest -t vilisseranen/castellers:`cat VERSION` .'
--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,13 @@
 
 # Push latest image to docker hub
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
-docker push vilisseranen/castellers:$1
 
-# Deploy on server
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no travis-deploy@carl.myhypervisor.ca -p 274 'cd /data/docker-compose/castellers && sudo docker-compose pull && sudo docker-compose up -d'
+if [ "$1" = "dev" ]; then
+    docker push vilisseranen/castellers:dev
+fi
+
+if [ "$1" = "latest" ]; then
+    docker push vilisseranen/castellers:latest
+    docker push vilisseranen/castellers:`cat VERSION`
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no travis-deploy@carl.myhypervisor.ca -p 274 'cd /data/docker-compose/castellers && sudo docker-compose pull && sudo docker-compose up -d'
+fi
