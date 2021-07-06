4b42f81bd31cd633471378e6a552c5dbe56dcac6
Clément Contini <vilisseranen@gmail.com>
2021-07-06 19:00:23 -0400

    create castells with people

--- controller/castells.go
+++ controller/castells.go
@@ -2,9 +2,11 @@ package controller
 
 import (
 	"database/sql"
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/vilisseranen/castellers/common"
 	"github.com/vilisseranen/castellers/model"
 )
 
@@ -37,11 +39,73 @@ func GetCastellTypeList(w http.ResponseWriter, r *http.Request) {
 	RespondWithJSON(w, http.StatusOK, castell_type_list)
 }
 
+func CreateCastellModel(w http.ResponseWriter, r *http.Request) {
+	var c model.CastellModel
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&c); err != nil {
+		common.Debug("Error decoding castell: %s", err.Error())
+		RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+	c.UUID = common.GenerateUUID()
+	// Create the model now
+	if err := c.Create(); err != nil {
+		RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	RespondWithJSON(w, http.StatusCreated, c)
+}
+
+func EditCastellModel(w http.ResponseWriter, r *http.Request) {
+	var c model.CastellModel
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&c); err != nil {
+		common.Debug("Error decoding castell: %s", err.Error())
+		RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+	// Update the model now
+	if err := c.Edit(); err != nil {
+		RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	RespondWithJSON(w, http.StatusAccepted, c)
+}
+
+func GetCastellModels(w http.ResponseWriter, r *http.Request) {
+	m := model.CastellModel{}
+	models, err := m.GetAll()
+	if err != nil {
+		switch err {
+		default:
+			RespondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	RespondWithJSON(w, http.StatusOK, models)
+}
+
 func GetCastellModel(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	castell_name := vars["name"]
-	castell_model := model.CastellModel{Name: castell_name}
-	if err := castell_model.Get(); err != nil {
+	castell_uuid := vars["uuid"]
+	m := model.CastellModel{UUID: castell_uuid}
+	if err := m.Get(); err != nil {
+		switch err {
+		default:
+			RespondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	RespondWithJSON(w, http.StatusOK, m)
+}
+
+func DeleteCastellModel(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	castell_uuid := vars["uuid"]
+	m := model.CastellModel{UUID: castell_uuid}
+	if err := m.Get(); err != nil {
 		switch err {
 		case sql.ErrNoRows:
 			RespondWithError(w, http.StatusNotFound, "Castell not found")
@@ -50,5 +114,9 @@ func GetCastellModel(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	RespondWithJSON(w, http.StatusOK, castell_model)
+	if err := m.Delete(); err != nil {
+		RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	RespondWithJSON(w, http.StatusOK, nil)
 }
--- model/castells.go
+++ model/castells.go
@@ -7,9 +7,12 @@ import (
 )
 
 const (
-	CASTELLTYPESVIEW   = "castell_types_view"
-	CASTELLTYPESTABLE  = "castell_types"
-	CASTELLMODELSTABLE = "castell_models"
+	CASTELLTYPESVIEW            = "castell_types_view"
+	CASTELLTYPESTABLE           = "castell_types"
+	CASTELLPOSITIONSTABLE       = "castell_positions"
+	CASTELLMODELSTABLE          = "castell_models"
+	CASTELLMODELSVIEW           = "castell_models_view"
+	CASTELLMEMBERPOSITIONSTABLE = "castell_members_positions"
 )
 
 type CastellType struct {
@@ -19,20 +22,21 @@ type CastellType struct {
 
 type CastellPosition struct {
 	Name   string `json:"name"`
-	Column string `json:"column"`
-	Cordon string `json:"cordon"`
+	Column int    `json:"column"`
+	Cordon int    `json:"cordon"`
 	Part   string `json:"part"`
 }
 
 type CastellModel struct {
+	UUID            string                   `json:"uuid"`
 	Name            string                   `json:"name"`
 	Type            string                   `json:"type"` // Will be the name of the castell type, ie: 3d6
-	PositionMembers []CastellPositionMembers `json:"position_members"`
+	PositionMembers []CastellPositionMembers `json:"position_members,omitempty"`
 }
 
 type CastellPositionMembers struct {
-	MemberUUID string `json:"member_uuid"`
-	PositionID int    `json:"position_id"`
+	MemberUUID string          `json:"member_uuid"`
+	Position   CastellPosition `json:"position"`
 }
 
 func (c *CastellType) Get() error {
@@ -78,6 +82,188 @@ func (c *CastellType) GetTypeList() ([]string, error) {
 	return castell_types, err
 }
 
+func (c *CastellModel) Create() error {
+	tx, err := db.Begin()
+	if err != nil {
+		common.Error("%v\n", err)
+		return err
+	}
+	stmt, err := tx.Prepare(fmt.Sprintf(
+		"INSERT INTO %s (uuid, name, castell_type_name) VALUES (?, ?, ?)",
+		CASTELLMODELSTABLE))
+	defer stmt.Close()
+	if err != nil {
+		common.Error("%v\n", err)
+		return err
+	}
+	_, err = stmt.Exec(
+		c.UUID,
+		c.Name,
+		c.Type,
+	)
+	if err != nil {
+		common.Error("%v\n", err)
+		return err
+	}
+	// For each position, add it in the table
+	for _, member := range c.PositionMembers {
+		stmt, err = tx.Prepare(fmt.Sprintf(
+			"INSERT INTO %s "+
+				"(castell_model_id, castell_position_id, member_id) VALUES ("+
+				"(SELECT id FROM %s WHERE uuid = ?), "+
+				"(SELECT id FROM %s WHERE name = ? AND column = ? AND cordon = ? AND part = ?), "+
+				"(SELECT id FROM %s WHERE uuid = ?))",
+			CASTELLMEMBERPOSITIONSTABLE, CASTELLMODELSTABLE, CASTELLPOSITIONSTABLE, MembersTable))
+		defer stmt.Close()
+		if err != nil {
+			common.Error("%v\n", err)
+			return err
+		}
+		_, err = stmt.Exec(
+			c.UUID,
+			member.Position.Name,
+			member.Position.Column,
+			member.Position.Cordon,
+			member.Position.Part,
+			member.MemberUUID,
+		)
+		if err != nil {
+			common.Error("%v\n", err)
+			return err
+		}
+	}
+	err = tx.Commit()
+	return err
+}
+
+func (c *CastellModel) Edit() error {
+	// Delete all positions
+	tx, err := db.Begin()
+	if err != nil {
+		common.Error("%v\n", err)
+		return err
+	}
+	stmt, err := tx.Prepare(fmt.Sprintf(
+		"DELETE FROM %s WHERE castell_model_id = (SELECT id FROM %s WHERE uuid = ?)",
+		CASTELLMEMBERPOSITIONSTABLE, CASTELLMODELSTABLE))
+	defer stmt.Close()
+	if err != nil {
+		common.Error("%v\n", err)
+		return err
+	}
+	_, err = stmt.Exec(
+		c.UUID,
+	)
+	if err != nil {
+		common.Error("%v\n", err)
+		return err
+	}
+	// add all positions
+	for _, member := range c.PositionMembers {
+		stmt, err = tx.Prepare(fmt.Sprintf(
+			"INSERT INTO %s "+
+				"(castell_model_id, castell_position_id, member_id) VALUES ("+
+				"(SELECT id FROM %s WHERE uuid = ?), "+
+				"(SELECT id FROM %s WHERE name = ? AND column = ? AND cordon = ? AND part = ?), "+
+				"(SELECT id FROM %s WHERE uuid = ?))",
+			CASTELLMEMBERPOSITIONSTABLE, CASTELLMODELSTABLE, CASTELLPOSITIONSTABLE, MembersTable))
+		defer stmt.Close()
+		if err != nil {
+			common.Error("%v\n", err)
+			return err
+		}
+		_, err = stmt.Exec(
+			c.UUID,
+			member.Position.Name,
+			member.Position.Column,
+			member.Position.Cordon,
+			member.Position.Part,
+			member.MemberUUID,
+		)
+		if err != nil {
+			common.Error("%v\n", err)
+			return err
+		}
+	}
+	// update model fields
+	stmt, err = tx.Prepare(fmt.Sprintf(
+		"UPDATE %s SET name=?, castell_type_name=? WHERE uuid=?",
+		CASTELLMODELSTABLE))
+	defer stmt.Close()
+	if err != nil {
+		common.Error("%v\n", err)
+		return err
+	}
+	_, err = stmt.Exec(
+		c.Name,
+		c.Type,
+		c.UUID,
+	)
+	if err != nil {
+		common.Error("%v\n", err)
+		return err
+	}
+	err = tx.Commit()
+	return err
+}
+
+func (c *CastellModel) GetAll() ([]CastellModel, error) {
+	rows, err := db.Query(fmt.Sprintf(
+		"SELECT uuid, name, castell_type_name FROM %s WHERE deleted=0",
+		CASTELLMODELSTABLE))
+	if err != nil {
+		common.Fatal(err.Error())
+	}
+	defer rows.Close()
+
+	models := []CastellModel{}
+
+	for rows.Next() {
+		var c CastellModel
+		if err = rows.Scan(&c.UUID, &c.Name, &c.Type); err != nil {
+			return nil, err
+		}
+		models = append(models, c)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return models, nil
+}
+
 func (c *CastellModel) Get() error {
+	stmt, err := db.Prepare(fmt.Sprintf(
+		"SELECT model_name, model_type, position_in_castell_name, position_in_castell_column, position_in_castell_cordon, position_in_castell_part, member_uuid FROM %s WHERE model_uuid = ? AND model_deleted=0",
+		CASTELLMODELSVIEW))
+	if err != nil {
+		common.Fatal(err.Error())
+	}
+	rows, err := stmt.Query(c.UUID)
+	defer rows.Close()
+	if err != nil {
+		common.Fatal(err.Error())
+	}
+	for rows.Next() {
+		var p CastellPositionMembers
+		if err = rows.Scan(&c.Name, &c.Type, &p.Position.Name, &p.Position.Column, &p.Position.Cordon, &p.Position.Part, &p.MemberUUID); err != nil {
+			return err
+		}
+		c.PositionMembers = append(c.PositionMembers, p)
+	}
+	if err = rows.Err(); err != nil {
+		return err
+	}
 	return nil
+
+}
+
+func (c *CastellModel) Delete() error {
+	stmt, err := db.Prepare(fmt.Sprintf("UPDATE %s SET deleted=1 WHERE uuid=?",
+		CASTELLMODELSTABLE))
+	defer stmt.Close()
+	if err != nil {
+		common.Fatal(err.Error())
+	}
+	_, err = stmt.Exec(c.UUID)
+	return err
 }
