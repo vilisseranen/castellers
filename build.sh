5b15fb36573daa2a383a40ca2835b3e70932952f
Clément Contini <vilisseranen@gmail.com>
2022-06-17 22:00:09 -0400

    fix deployment files

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Building docker image..."
-sudo docker build -t vilisseranen/castellers:latest -t vilisseranen/castellers:dev -t vilisseranen/castellers:latest -t vilisseranen/castellers:`cat VERSION` .
