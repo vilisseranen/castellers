bc2940fc88b7e62d8b5372cd1e46f77827e58df5
Clément Contini <vilisseranen@gmail.com>
2022-07-12 21:19:04 -0400

    set DB name by env variable in litestream

--- deployment/fly.io/scripts/run.sh
+++ deployment/fly.io/scripts/run.sh
@@ -2,12 +2,12 @@
 set -e
 
 # Restore the database if it does not already exist.
-if [ -f /data/castellers.db ]; then
+if [ -f ${APP_DB_NAME} ]; then
 	echo "Database already exists, skipping restore"
 else
 	echo "No database found, restoring from replica if exists"
-	litestream restore -v -if-replica-exists -o /data/castellers.db "${REPLICA_URL}"
+	litestream restore -v -if-replica-exists -o ${APP_DB_NAME} "${REPLICA_URL}"
 fi
 
 # Run litestream with your app as the subprocess.
-exec litestream replicate -exec "/app -dsn /data/castellers.db"
+exec litestream replicate -exec "/app -dsn ${APP_DB_NAME}"
